47e0f90453abe7f50d3aad44d23090d74c8f5f5e
maksim-nevekin <maksim.new99@gmail.com>
2024-04-23 15:43:57 +0300

    Transfer the general parameters of the models to the base abstract class

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Customer, Executor
 
-# Register your models here.
+
+admin.site.register(Customer)
+admin.site.register(Executor)
--- profiles/models.py
+++ profiles/models.py
@@ -2,7 +2,7 @@ from datetime import timezone
 from django.db import models
 
 
-class Customer(models.Model):
+class BaseModel(models.Model):
     name = models.CharField(max_length=50, verbose_name="Имя")
     second_name = models.CharField(max_length=50, verbose_name="Фамилия")
     phone = models.IntegerField(verbose_name="Номер телефона")
@@ -15,21 +15,22 @@ class Customer(models.Model):
         return f"{self.name[0]}.{self.second_name}"
 
     class Meta:
+        abstract = True
+
+
+class Customer(BaseModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    class Meta:
         abstract = False
         verbose_name ='заказчика'
         verbose_name_plural ='Заказчики'
 
-class Executor(models.Model):
-    name = models.CharField(max_length=50, verbose_name="Имя")
-    second_name = models.CharField(max_length=50, verbose_name="Фамилия")
-    phone = models.IntegerField(verbose_name="Номер телефона")
-    email = models.EmailField(max_length=254, verbose_name="Адрес электронной почты")
-    experience = models.IntegerField(verbose_name="Опыт работы")
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
 
-    def __str__(self) -> str:
-        return f"{self.name[0]}.{self.second_name}"
+class Executor(BaseModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     class Meta:
         abstract = False
