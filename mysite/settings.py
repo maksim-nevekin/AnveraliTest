9084bd3e2c2529902dc2dfee909b1b010d215567
maksim-nevekin <maksim.new99@gmail.com>
2024-04-25 17:18:50 +0300

    Create main application

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import os
 from decouple import config
 
 
@@ -42,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'profiles',
+    'main',
 ]
 
 MIDDLEWARE = [
--- profiles/views.py
+++ profiles/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'profiles/forms.html')
+    return render(request, 'base.html')
